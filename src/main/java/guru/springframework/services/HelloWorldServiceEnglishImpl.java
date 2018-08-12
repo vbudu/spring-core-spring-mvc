d26e4b63b46a91c0b065a5de80d35e21f017d8cc
vlad_specter <vbudu.4p@gmail.com>
2018-08-13 00:18:07 +0100

    Spring Profiles With Dependency Injection - multiple profiles definition;

--- src/main/java/guru/springframework/services/HelloWorldServiceEnglishImpl.java
+++ src/main/java/guru/springframework/services/HelloWorldServiceEnglishImpl.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("default")
+@Profile({"default","english"})
 public class HelloWorldServiceEnglishImpl implements HelloWorldService {
 
     @Override
