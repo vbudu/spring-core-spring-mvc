7ba38aed57b082046f22531211e24d4aea9e9da8
vlad_specter <vbudu.4p@gmail.com>
2018-08-13 00:10:45 +0100

    Spring Profiles With Dependency Injection

--- src/main/java/guru/springframework/services/HelloWorldServiceEnglishImpl.java
+++ src/main/java/guru/springframework/services/HelloWorldServiceEnglishImpl.java
@@ -1,8 +1,10 @@
 package guru.springframework.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("english")
 public class HelloWorldServiceEnglishImpl implements HelloWorldService {
 
     @Override
--- src/main/java/guru/springframework/services/HelloWorldServiceSpanishImpl.java
+++ src/main/java/guru/springframework/services/HelloWorldServiceSpanishImpl.java
@@ -1,11 +1,13 @@
 package guru.springframework.services;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("spanish")
 public class HelloWorldServiceSpanishImpl implements HelloWorldService {
     @Override
     public String getGreeting() {
-        return "Ola mundo!";
+        return "Hola mundo!";
     }
 }
